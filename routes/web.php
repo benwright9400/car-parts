3f763cb54a0c8bab8d87c62c14ec5c0bdf8bbab0
Ben Wright <benwright9400@gmail.com>
2023-01-18 21:54:48 +0000

    create migrations; create models; map models to resource routes; switch to react for UI;

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ManufacturerController;
+use App\Http\Controllers\PartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/manufacturers',ManufacturerController::class);
+
+Route::resource('/parts',PartController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
